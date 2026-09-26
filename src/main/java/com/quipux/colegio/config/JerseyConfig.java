5ecd3d342c2f94720cbff374944a09ac04859a3c
thelayor <juanpablocardona082@gmail.com>
2026-09-26 04:53:49 +0000

    feat: completar retos 1-4 del backend

--- src/main/java/com/quipux/colegio/config/JerseyConfig.java
+++ src/main/java/com/quipux/colegio/config/JerseyConfig.java
@@ -3,16 +3,10 @@ package com.quipux.colegio.config;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
-import com.quipux.colegio.services.HechizoService;
-
 @Component
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
-        // Registrar el servicio/controlador para exponer la API
-        register(HechizoService.class);
-        register(com.quipux.colegio.services.MagoService.class);
-        
-        // Registrar recursos de OpenAPI/Swagger
+        packages("com.quipux.colegio.services");
         register(io.swagger.v3.jaxrs2.integration.resources.OpenApiResource.class);
     }
 }
